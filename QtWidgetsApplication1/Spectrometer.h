2b38be70505dcd5f9586c8a7bc52a7bb6562e340
Sergey Kozlov <sergey782805@gmail.com>
2026-05-13 09:47:46 +0300

    WIP: version 2 of auto adjustments for dark spectrum using bias dark spectrum, calib dark spectrum. reading 2 dark spectrums

--- QtWidgetsApplication1/Spectrometer.h
+++ QtWidgetsApplication1/Spectrometer.h
@@ -19,21 +19,30 @@ public:
 	const unsigned long getDarkIntegrationTime();
 	const unsigned long getMaxIntegrationTime();
 	const unsigned long getMinIntegrationTime();
+	std::vector<double> getBiasDarkSpectrum();
+	std::vector<double> getPureDarkSpectrum();
 	std::vector<double> getDarkSpectrum();
 	const unsigned long detectIntegrationTime();
 	const bool isReady();
 	std::vector<double> readWaveLengths();
+	std::vector<double> readBiasDarkSpectrum();
+	std::vector<double> readCalibDarkSpectrum();
+	std::vector<double> calcPureDark();
 	std::vector<double> readDarkSpectrum();
 	std::vector<double> readCorrectedSpectrum();
 	std::size_t getIndexOfWavelenght(double wavelength);
 	void setIntegrationTime(const unsigned long ms); // ??
 	void setAverageFactor(const unsigned int average); //?
+	
 	void setDarkSpectrum(const std::vector<double>& darkSpectrum);
 	void setDarkIntegrationTime(const unsigned long newTime);
 	
 private:
 	std::vector<double> m_wavelengths;
-	std::vector<double> m_darkSpectrum;
+	std::vector<double> m_biasDarkSpectrum;
+	std::vector<double> m_calibDarkSpectrum;
+	std::vector<double> m_pureDarkSpectrum;
+	std::vector<double> m_darkSpectrum; // pure dark
 	std::vector<double> m_correctedSpectrum;
 	std::vector<long> m_deviceIds;
 	unsigned long m_integrationTimeMicroseconds;
